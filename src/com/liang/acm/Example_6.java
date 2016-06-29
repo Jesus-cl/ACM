ad6cde5208f5c91c1ec8375ca7c979ff93e9a9f3
chenliang <clyancl@163.com>
2016-06-30 01:39:41 +0800

    update

--- src/com/liang/acm/Example_6.java
+++ src/com/liang/acm/Example_6.java
@@ -2,17 +2,25 @@ package com.liang.acm;
 
 import java.util.Scanner;
 
+/*
+ * @author chenliang 
+ * 现有一块草坪，长为20米，宽为2米，要在横中心线上放置半径为Ri的喷水装置，
+ * 每个喷水装置的效果都会让以它为中心的半径为实数Ri(0<Ri<15)的圆被湿润，
+ * 这有充足的喷水装置i（1<i<600)个，并且一定能把草坪全部湿润， 
+ * 你要做的是：选择尽量少的喷水装置，把整个草坪的全部湿润。
+ */
 public class Example_6 {
-
+	/*
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner in=new Scanner(System.in);
-		int m=in.nextInt();
+		Scanner in = new Scanner(System.in);
+		int m = in.nextInt();
 		System.out.println("Please input m:");
-		for(int i=0;i<m;i++){
-			
+		for (int i = 0; i < m; i++) {
+
 		}
-		
+
 	}
 
 }
